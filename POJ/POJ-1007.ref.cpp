3755a7faca32aad9b112d6074d961484f77d7e2a
lightyears1998 <lightyears1998@hotmail.com>
2018-02-20 11:03:20 +0800

    0220

---
+++ POJ/POJ-1007.ref.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+// ”≥…‰
+// https://vjudge.net/solution/1051124
+
+char str[100][55];
+int ref[100];
+
+int main()
+{
+	int n, m; scanf("%d%d", &n, &m);
+	for (int i = 0; i < m; ++i)
+	{
+		scanf("%s", str[i]);
+		for (int j = 0; j < n; ++j)
+			for (int k = j+1; k < n; ++k) if (str[i][j] > str[i][k])
+				++ref[i];
+		ref[i] = 100*ref[i] + i;
+	}
+	sort(ref, ref+m);
+	for (int i = 0; i < m; ++i)
+		puts(str[ref[i]%100]);
+} 
+
